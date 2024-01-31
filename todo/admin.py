d8d8bf2f0ce96915ac3591780ac7be59daedaa5c
shayan <137724478+Sh0yanFarahani@users.noreply.github.com>
2024-01-31 14:48:33 +0330

    add model todo

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ('title',)
+    list_display = ["title", "complete"]
+    fields = ["title", "complete"]
+    list_filter = ("title",)
+
+
+admin.site.register(Task, TaskAdmin)
